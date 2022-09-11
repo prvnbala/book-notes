e5c977e27e3814bab3f2eaf6a1aec587aafca722
praveen <praveen.balasundram@gmail.com>
2022-09-11 22:41:49 +0530

    add multi color support to gif file

--- the-go-programming-language/ch1/lissajous.go
+++ the-go-programming-language/ch1/lissajous.go
@@ -11,15 +11,26 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0xff, 0xff, 0x00, 0xff},
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+}
 
+// create constants
 const (
 	whiteIndex = 0 //first color in palette
 	blackIndex = 1 //next color in palette
 )
 
 func main() {
-	lissajous(os.Stdout)
+	//create a file for output gif image
+	f, err := os.Create("out.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	lissajous(f)
 }
 
 func lissajous(out io.Writer) {
@@ -28,7 +39,7 @@ func lissajous(out io.Writer) {
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
+		delay   = 8     // delay between frames in 80ms units
 	)
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
@@ -36,10 +47,15 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
+		colorIndex := 1
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(colorIndex))
+			colorIndex++
+			if colorIndex > len(palette) {
+				colorIndex = 1
+			}
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
