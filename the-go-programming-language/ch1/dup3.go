63844ac784e1bfe43906823d0f8656a48dc7d062
praveen <praveen.balasundram@gmail.com>
2022-09-11 17:22:41 +0530

    Add new files

---
+++ the-go-programming-language/ch1/dup3.go
@@ -0,0 +1,27 @@
+// dup3 prints the count and lines in a file that occurs more than
+// once. It reads the entire file into memory at once.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		fmt.Printf("%d\t%s\n", n, line)
+	}
+}
