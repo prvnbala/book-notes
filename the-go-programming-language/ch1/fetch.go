ff6e3db8baa9fa82f550ff968c23ad06e72d7f0c
praveen <praveen.balasundram@gmail.com>
2022-09-12 07:54:10 +0530

    Add method 2 for printing http content.

--- the-go-programming-language/ch1/fetch.go
+++ the-go-programming-language/ch1/fetch.go
@@ -3,24 +3,37 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
+
+		//method 1
+		// b, err := ioutil.ReadAll(resp.Body)
+		// resp.Body.Close()
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		// 	os.Exit(1)
+		// }
+		// fmt.Printf("%s", b)
+
+		//print HTTP status
+		fmt.Println("HTTP Status: ", resp.Status)
+
+		//method 2
+		io.Copy(os.Stdout, resp.Body)
 	}
 }
